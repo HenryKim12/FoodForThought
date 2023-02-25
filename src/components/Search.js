250e1af650e7e1e89d2117b7f43f6e8ef9e9a145
Henry <henryshkim@hotmail.com>
2023-02-25 15:21:59 -0800

    before refactoring code

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,13 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import RecipeAPI from './RecipeAPI';
 import RecipeList from './RecipeList';
-// import { Button } from 'react-bootstrap';
 
 export default function Search(props) {
     const [food, setFood] = useState("");
     const [recipes, setRecipes] = useState([]);
+    const [links, setLinks] = useState([]);
     let res = [];
-    // const [update, setUpdate] = useState(false);
 
     const getSearch = (event) => {
         const searched = event.target.value;
@@ -35,25 +34,26 @@ export default function Search(props) {
         };
 
         let response = await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${props.food}/`, options)
-        // let response = await fetch("https://api.spoonacular.com/mealplanner/generate?apiKey=26f3103038msh83c948ed7470bdcp1f3303jsnf9e4439a1ecb");
         let data = await response.json();
 
-        // for (let i = 0; i < data.results.length; i++) {
-        //     recipes.push(data.results[i])
-        // }
-        // console.log(data.results[0])
         setRecipes(data.results);
-        // console.log({recipes});
-
-        // console.log(res);
-        // setRecipes(recipes => [...recipes, data.results]);
-        // console.log(data.results);
-        // console.log({recipes})
     }
 
-    // async function fetchRecipesList() {
-    //     let response = await fetch(`https://api.spoonacular.com/recipes/${}/information`)
-    // }
+    async function fetchActualRecipe(id) {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '26f3103038msh83c948ed7470bdcp1f3303jsnf9e4439a1ecb',
+                'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+            }
+        };
+        
+        let response = await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`, options)
+        let data = await response.json();
+        // console.log(data);
+        console.log(data.sourceUrl);
+        setLinks(data.sourceUrl);
+    }
 
     return (
         <div className='searchPage'>
@@ -62,21 +62,24 @@ export default function Search(props) {
                     <div className='searchBar'>
                         <Form.Label className='label'>Recipe</Form.Label>
                         <input type="text" placeholder='Enter food (ie. pasta)' onChange={getSearch} />
-                        {/* <Button variant="primary" type="submit">Submit</Button> */}
-                        {/* <button className="submitButton" onClick={fetchRecipes}>Submit</button> */}
                         <Form.Text className="text-muted">
                         Enter the name of food you want ie. pasta
                         </Form.Text>
                     </div>
                 </Form.Group>
             </Form>
-                <Button className="submitButton" onClick={fetchRecipes}>Submit</Button>
+            <Button className="submitButton" onClick={fetchRecipes}>Submit</Button>
             <div className="listofRecipes">
                 {recipes.map(recipe => 
+                <div>
                     <div className='recipeInfo'>
                         {recipe.title}
                         <img src={recipe.image}/>
+                        <Button onClick={fetchActualRecipe(recipe.id)}>Go to recipe</Button>
+                        <a href={links}>Go to recipe</a>
+                        {/* <a>{links}</a> */}
                     </div>
+                </div>
                 )}
             </div>
         </div>
